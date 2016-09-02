ed82bf64e72ba66d8fd7265c9492e5feafde3773
Stu Radnidge <sturadnidge@pivotal.io>
2016-09-02 10:12:51 +1000

    handle args differently

--- pcf-start-stop.sh
+++ pcf-start-stop.sh
@@ -7,12 +7,14 @@
 shopt -s expand_aliases
 source ~/.profile
 
-if [ $1 == "stop" -o $1 == "start" ];
-  then
-    echo "Running PCF $1 Process..."
-  else
-    echo "Usage: ${0##*/} [stop | start]"
-    exit 1
+function usage {
+  echo "Usage: ${0##*/} [stop | start]"
+  exit 1
+}
+
+if [ $# -ne 1 ]
+then
+  usage
 fi
 
 # These must be explicitly stopped due to nfs_mounter blocking as of 1.6.17
@@ -22,25 +24,32 @@ declare -a bootOrder=(
   cloud_controller_worker
 )
 
-if [ $1 == "stop" ]; then
-  jobVMs=$(bosh vms --detail| grep partition| awk -F '|' '{ print $2 }')
-  bosh vm resurrection off
-  for (( i=${#bootOrder[@]}-1; i>=0; i-- )); do
-    for x in $jobVMs; do
-      jobId=$(echo $x | awk -F "/" '{ print $1 }')
-      instanceId=$(echo $x | awk -F "/" '{ print $2 }')
-      jobType=$(echo $jobId | awk -F "-" '{ print $1 }')
-        if [ "$jobType" == "${bootOrder[$i]}" ];
-        then
-          echo "stopping $jobType ($jobId/$instanceId)"
-          bosh -n stop $jobId --hard
-        fi
-    done;
-  done;
-  bosh -n stop --hard
-fi
+case $1 in
 
-if [ $1 == "start" ]; then
-  bosh -n start
-  bosh vm resurrection on
-fi
+  'stop')
+    jobVMs=$(bosh vms --detail| grep partition| awk -F '|' '{ print $2 }')
+    bosh vm resurrection off
+    for (( i=${#bootOrder[@]}-1; i>=0; i-- )); do
+      for x in $jobVMs; do
+        jobId=$(echo $x | awk -F "/" '{ print $1 }')
+        instanceId=$(echo $x | awk -F "/" '{ print $2 }')
+        jobType=$(echo $jobId | awk -F "-" '{ print $1 }')
+          if [ "$jobType" == "${bootOrder[$i]}" ];
+          then
+            echo "stopping $jobType ($jobId/$instanceId)"
+            bosh -n stop $jobId --hard
+          fi
+      done;
+    done;
+    bosh -n stop --hard
+    ;;
+
+  'start')
+    bosh -n start
+    bosh vm resurrection on
+    ;;
+
+  *)
+   usage
+
+esac
