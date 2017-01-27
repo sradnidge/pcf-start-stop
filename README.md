b066258c0f38cfded2938c8e938ebec9edc7e73a
Stu Radnidge <sturadnidge@pivotal.io>
2017-01-27 14:57:55 +1100

    make hard stop optional, keep file server and database jobs if they exist

--- README.md
+++ README.md
@@ -4,10 +4,8 @@
 - SSH into Ops Manager
 - Log in to your BOSH director (`bosh --ca-cert /var/tempest/workspaces/default/root_ca_certificate target DIRECTOR-IP-ADDRESS`)
 - Target your cf deployment (`bosh deployment DEPLOYMENT-FILENAME.yml`)
-- Run `pcf-aws-start-stop.sh`
+- Run `pcf-start-stop.sh (start | stop) [--hard]`
  - Optionally, add to crontab to schedule
 
 ## WARNING
-Think about what the primary purpose of this script is: to save money by shutting things down when you don't need them. It assumes your deployment is using external blob storage (eg S3) and an external database (eg RDS), which will retain the state of your CF deployment even if every single CF VM is destroyed and re-created (note that Ops Manager and BOSH are not part of a cf manifest, so they will not be deleted by this script). If you chose the 'internal' options for those things, running this script will indeed result in the loss of everything.
-
-If you don't like this behaviour, use the fork!
+If you chose the 'internal' options for blob store and / or database when deploying PCF, running this script with the `--hard` option will indeed result in the loss of everything.
---
+++ pcf-start-stop.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# bosh cmd is an alias set in .profile of ops manager user
+shopt -s expand_aliases
+source ~/.profile
+
+function usage {
+  echo "Usage: ${0##*/} (start | stop) [--hard]"
+  exit 1
+}
+
+if [ $# -eq 0 ]
+  then
+    usage
+fi
+
+# @param Array
+# @param Element
+# @usage hasIn "${Array[@]}" "Element"
+function hasIn {
+  local e
+  for e in "${@:1}"; do [[ "$e" == "$2" ]] && return 0; done
+  return 1
+}
+
+# Get a friendly list of jobs
+jobVMs=$(bosh vms --detail| grep partition| awk -F '|' '{ print $2 }')
+
+# If doing a hard stop, do not delete these jobs (if they exist)
+declare -a doNotDelete=(
+  mysql
+  nfs_server
+)
+
+case $1 in
+
+  'start')
+    bosh -n start
+    bosh vm resurrection on
+    ;;
+
+  'stop')
+    bosh vm resurrection off
+    # stop everything except jobs in doNotStop
+    for x in $jobVMs; do
+      jobId=$(echo $x | awk -F "/" '{ print $1 }')
+      instanceId=$(echo $x | awk -F "/" '{ print $2 }')
+      jobType=$(echo $jobId | awk -F "-" '{ print $1 }')
+      if [ -n "$2" ] && [ "$2" == "--hard" ]
+        then
+          if [ hasIn "${doNotDelete[@]}" "$jobType" ]
+            then
+              echo "stopping $jobType ($jobId/$instanceId)"
+              bosh -n stop $jobId
+            else
+              echo "deleting $jobType ($jobId/$instanceId)"
+              bosh -n stop $jobId --hard
+          fi
+        else
+          echo "stopping $jobType ($jobId/$instanceId)"
+          bosh -n stop $jobId
+      fi
+
+    done;
+    ;;
+
+  *)
+    usage
+    ;;
+
+esac
